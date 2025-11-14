1b2d54bea935e5ab126fc93de105602c233a1692
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-14 10:25:19 +0700

    add post_gcd status

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,4 +1,11 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -6,6 +13,21 @@ async fn main() -> std::io::Result<()> {
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+
+        m = m % n;
+    }
+    n
 }
 
 async fn get_index() -> impl Responder {
@@ -19,4 +41,21 @@ async fn get_index() -> impl Responder {
     </form>
     "#,
     )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the number {} and {}is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
