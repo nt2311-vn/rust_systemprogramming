51c1f3463e5bc34441b40d9ce5e92d821892137c
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-14 15:01:05 +0700

    write parse_pair function

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,5 @@
+use std::{str::FromStr, usize};
+
 use num::Complex;
 fn main() {
     println!("Hello, world!");
@@ -23,3 +25,47 @@ fn complex_square_add_loop(c: Complex<f64>) {
         z = z * z + c;
     }
 }
+
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// integrations it took for `c` to leave the circle of radius 2 centered on
+/// the origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member)
+/// return `None`.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+
+        z = z * z + c;
+    }
+
+    None
+}
+
+/// Parse the string `s` as coordinate pair, like `"400 x 600"` or `"1.0,0.5"`
+///
+/// Specifically, `s` should have form <left><sep><right>, where <sep> is
+/// the character given by the `seperator` argument, and <left> and <right> are
+/// both strings that can be parsed by `T::from_str`. `seperator` must be an
+/// ASCII character.
+///
+/// If `s` has the proper form, return `Some(x, y)`. If it doesn't parse
+/// correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
+    match s.find(seperator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {}
