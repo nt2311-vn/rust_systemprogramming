4dfcbafa35cf2c7bec8181b26501dc5a884ebaf8
ntcachyos <nguyentoan231196@gmail.com>
2025-11-15 21:03:33 +0700

    write a render method to render in pixels

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -139,3 +139,29 @@ fn test_pixel_to_point() {
         }
     );
 }
+
+/// Render a rectangle of Mandelbrot set into a buffer of pixels
+///
+/// The `bounds` argument give thheh height and width of the buffer `pixels`,
+/// which holds one grayscale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on thhe complex plane corresponding the upper_left
+/// and lower_right corner of the pixel buffer.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
