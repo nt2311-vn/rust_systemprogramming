c9388d7a41107ebb168a341eeaac958b45feb6d6
ntcachyos <nguyentoan231196@gmail.com>
2025-11-15 09:25:39 +0700

    write a function pixel to point

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,7 +1,7 @@
 use core::f64;
 use std::{str::FromStr, string::ParseError, usize};
 
-use num::Complex;
+use num::{Complex, complex::ComplexFloat};
 fn main() {
     println!("Hello, world!");
 }
@@ -98,4 +98,28 @@ fn test_parse_complex() {
         })
     );
     assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane
+///
+/// `bounds` is a pair giving the height and width of the image in pixels.
+///  `pixel` is a (column, row) pair indicating a particular pixel in that image.
+///  The `upper_left` and `lower right` parameters are points on the complex
+///  plane designating the area our image covers.
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im + pixel.1 as f64 * height / bounds.1 as f64,
+    }
 }
