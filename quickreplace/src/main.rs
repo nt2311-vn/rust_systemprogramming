9f19ba1934694283ea13205d79d79919f9c9802e
ntcachyos <nguyentoan231196@gmail.com>
2025-11-16 09:54:30 +0700

    write function parse argument and retun a struct of Arguments

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -16,5 +18,28 @@ fn print_usage() {
     eprintln!(
         "{} - change occurences of one string to another",
         "quickreplace".green()
-    )
+    );
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "Error".red().bold(),
+            args.len()
+        );
+
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
 }
