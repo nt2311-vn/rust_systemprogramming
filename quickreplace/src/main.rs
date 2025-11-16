7ebcdb18b478e5b2b8765ed9b324cc760c871263
ntcachyos <nguyentoan231196@gmail.com>
2025-11-16 21:05:05 +0700

    add the data after parse the arguments success and try to find and replace Cargo.toml with a file Copy.toml

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{
+    env::{self, args},
+    fs,
+};
 
 use text_colorizer::*;
 
@@ -13,6 +16,34 @@ struct Arguments {
 fn main() {
     let args = parse_args();
     println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error".red().bold(),
+                args.filename,
+                e
+            );
+
+            std::process::exit(1);
+        }
+    }
 }
 
 fn print_usage() {
