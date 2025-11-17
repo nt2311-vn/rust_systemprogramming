868abd2f1132a5bd32e51d0734c4e708cec9a4cd
ntcachyos <nguyentoan231196@gmail.com>
2025-11-17 21:52:21 +0700

    add simple command info of author and what about the app with clap::Command::new

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = Command::new("echor")
+        .version("0.1.0")
+        .author("nt2311-vn")
+        .about("Rust echo")
+        .get_matches();
 }
