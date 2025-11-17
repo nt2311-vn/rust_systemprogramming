20fa7b1bc5f9dd1c8ae12db4167f19ff5d12a753
ntcachyos <nguyentoan231196@gmail.com>
2025-11-17 22:08:47 +0700

    update some arguments to the app

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,22 @@
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
     let _matches = Command::new("echor")
         .version("0.1.0")
         .author("nt2311-vn")
         .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print new line")
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
 }
