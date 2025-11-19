77c01b4fd10b265b87f5ecbf73f005d88dc79b19
ntcachyos <nguyentoan231196@gmail.com>
2025-11-19 21:44:04 +0700

    update function catr in rust

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -19,4 +20,45 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
-fn main() {}
+fn main() {
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{filename}: {e}"),
+            Ok(file) => {
+                let mut prev_num = 0;
+
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if args.number_lines {
+                        println!("{:6}\t{line}", line_num + 1);
+                    } else if args.number_nonblank_lines {
+                        if line.is_empty() {
+                            println!()
+                        } else {
+                            prev_num += 1;
+                            println!("{prev_num:6}\t{line}");
+                        }
+                    } else {
+                        println!("{line}");
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
