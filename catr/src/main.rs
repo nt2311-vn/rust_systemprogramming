c329c54d6ff67b02cb9d3352b4494cc2afabeb59
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-19 10:49:59 +0700

    define public function run and use in main entry point

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,15 @@
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
 fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+pub fn run() -> MyResult<()> {
     println!("Hello, world!");
+    Ok(())
 }
