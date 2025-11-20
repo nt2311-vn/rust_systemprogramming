a29383e59586f3e6ee5ca1f396d36059ed1ea139
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-20 14:20:28 +0700

    update cli cargo test

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,7 +1,6 @@
-use std::{error, fs};
+use std::fs;
 
 use assert_cmd::Command;
-use predicates::prelude::*;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -26,10 +25,10 @@ fn runs() -> TestResult {
 
 #[test]
 fn hello1() -> TestResult {
-    let outfile = "./tests/expected/hello1.txt";
-    let _expected = fs::read_to_string(outfile).unwrap();
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
     let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello").assert().success().stdout("Hello\n");
+    cmd.arg("Hello there").assert().success().stdout(expected);
 
     Ok(())
 }
