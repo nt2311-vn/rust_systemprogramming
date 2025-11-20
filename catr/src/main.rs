fd842d2352d76ddd909064bd812c5caefee1a397
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-20 11:45:51 +0700

    write oldstyle of clap

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Arg, ArgAction, Command, Parser};
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -18,6 +20,28 @@ struct Args {
     /// Number non-blank lines
     #[arg(short('b'), long("number-nonblank"))]
     number_nonblank_lines: bool,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("nt2311-vn")
+        .about("Rust cat")
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("number lines")
+                .action(ArgAction::SetFalse)
+                .conflicts_with("number_nonblank"),
+        );
 }
 
 fn main() {
