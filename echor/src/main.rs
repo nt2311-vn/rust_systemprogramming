95c5f57fed2556619150d8d8f8210a2659e9256e
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-20 14:13:44 +0700

    refactor echor as new format of clap

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,34 +1,23 @@
-use clap::{Arg, ArgAction, Command, builder::ValueParser};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, author, about)]
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("nt2311-vn")
-        .about("Rust echo")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..)
-                .value_parser(ValueParser::os_string()),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .help("Do not print new line")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let text: Vec<String> = matches
-        .get_many::<std::ffi::OsString>("text")
-        .unwrap()
-        .map(|os_str| os_str.to_string_lossy().to_string())
-        .collect();
-
-    let omit_newline: bool = *matches.get_one::<bool>("omit_newline").unwrap_or(&false);
-    let ending = if omit_newline { "" } else { "\n" };
-
-    print!("{}{}", text.join(" "), ending);
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    )
 }
