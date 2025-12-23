3bee8f412e675b4abda5dc529cfac67f0c2af25c
nt2311-vn <nguyentoan231196@gmail.com>
2025-12-23 18:04:46 +0700

    write args struct for headr

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(default_value = "-", value_name = "FILE")]
+    files: Vec<String>,
+
+    #[arg(
+        short('n'),
+        long, default_value = "10",
+        value_name = "LINES",
+        value_parser = clap::value_parser!(u64).range(1..)
+    )]
+    lines: u64,
+
+    #[arg(
+        short('c'),
+        long,
+        value_name = "BYTES",
+        conflicts_with("lines"),
+        value_parser = clap::value_parser!(u64).range(1..)
+    )]
+    bytes: Option<u64>,
 }
+
+fn main() {}
